d686dc6c907b866c660fe9383c7a18dce9805eff
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-22 13:13:43 -0400

    fixed JavaScript Logic and added the h3 to display 'You both have a BMI... when both values are ===

--- script.js
+++ script.js
@@ -12,13 +12,17 @@ function bmiCalculator() {
     userBMI = (userMass / (userHeight ** 2)) * 703;
     friendBMI = (friendMass / (friendHeight ** 2)) * 703;
 
-    if (userBMI > friendBMI) {
+    if (userBMI === friendBMI){
+        document.querySelector("h3").textContent = `You both have a BMI of ${Math.round(userBMI)}`;
+    } 
+    
+    else if (userBMI > friendBMI) {
         document.querySelector("h3").textContent = `${user} has a higher BMI of ${Math.round(userBMI)}`;
-    } else if (userBMI < friendBMI) {
+    } 
+    else {
         document.querySelector("h3").textContent = `${friend} has a higher BMI of ${Math.round(friendBMI)}`;
-    } else {
-        `You both have a BMI of ${Math.round(userBMI)}`;
-    }
+    } 
+    
 }
 
 
